8b79e88ab98531d86f6de62941dc6ed0f1e2b9a1
Yuhahak <100738313+Yuhahak@users.noreply.github.com>
2024-12-05 08:42:36 +0900

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241205/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    int d = a+b+c;
+    cout << d << endl;
+    cout << d / 3 << endl;
+    cout << d - ( d/ 3 );
+    return 0;
+}
