b4cb8667aabf67de56d36e83b5836efea5c52aa5
Yuhahak <100738313+Yuhahak@users.noreply.github.com>
2024-12-10 08:27:28 +0900

    Title: 최댓값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241210/최댓값 구하기/maximum-value.cpp
+++ 241210/최댓값 구하기/maximum-value.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main() {
     int a ,b, c;
     cin >> a >> b >> c;
-    if(a > b && a > c){
+    if(a >= b && a >= c){
         cout << a;
     }
-    else if( b > a && b > c){
+    else if( b >= a && b >= c){
         cout << b;
     }
     else{
