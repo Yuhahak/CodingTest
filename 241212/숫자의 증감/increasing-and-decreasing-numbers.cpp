01e93f394c27806bd9cab22e7ac13b15e129017a
Yuhahak <100738313+Yuhahak@users.noreply.github.com>
2024-12-12 08:37:34 +0900

    Title: 숫자의 증감, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241212/숫자의 증감/increasing-and-decreasing-numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a;
+    int b;
+    cin >> a >> b;
+    if(a =='A'){
+        for(int i = 1; i <= b; i++){
+            cout << i  << " ";
+        }
+    }
+    else if(a == 'D'){
+        for(int i = b ; i >= 1 ; i--){
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
