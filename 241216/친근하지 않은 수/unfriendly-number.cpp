b5a5d935c9c7e565c295f8d81550044b08b81d7e
Yuhahak <100738313+Yuhahak@users.noreply.github.com>
2024-12-16 08:59:08 +0900

    Title: 친근하지 않은 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241216/친근하지 않은 수/unfriendly-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, cnt = 0;
+    cin >> a;
+    for(int i =1 ; i <= a ; i++){
+        if(i % 2 == 0 || i % 3 == 0 || i % 5 == 0){
+            continue;
+        }
+        cnt ++;
+    }
+    cout << cnt;
+    return 0;
+}
