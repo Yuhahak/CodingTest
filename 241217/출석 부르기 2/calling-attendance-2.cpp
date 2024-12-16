a22d2f136124b45392d037487298369b2487b347
Yuhahak <100738313+Yuhahak@users.noreply.github.com>
2024-12-17 08:36:34 +0900

    Title: 출석 부르기 2, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241217/출석 부르기 2/calling-attendance-2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    while(true){
+        cin >> a;
+        if(a == 1){
+            cout << "John" << endl;
+        }
+        else if(a == 2){
+            cout << "Tom" << endl;
+        }
+        else if(a == 3){
+            cout << "Paul" << endl;
+        }
+        else if(a == 4){
+            cout << "Sam" << endl;
+        }
+        else{
+            cout << "Vacancy";
+            break;
+        }
+    }
+    return 0;
+}
