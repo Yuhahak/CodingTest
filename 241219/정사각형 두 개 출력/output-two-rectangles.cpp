9a0ff573eb55f10bfef6bc757154574d7f9772d4
Yuhahak <100738313+Yuhahak@users.noreply.github.com>
2024-12-19 09:05:21 +0900

    Title: 정사각형 두 개 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241219/정사각형 두 개 출력/output-two-rectangles.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    for(int k = 1 ; k <= 2 ; k++){
+            for(int i = 1 ; i <= a; i ++){
+        for(int j = 1 ; j <= a; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+        cout << endl;
+    }
+    return 0;
+}
