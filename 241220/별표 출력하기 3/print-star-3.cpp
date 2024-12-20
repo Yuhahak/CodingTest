3dc30c4f16beda8d2b434f10a291e67200a867c7
Yuhahak <100738313+Yuhahak@users.noreply.github.com>
2024-12-20 09:19:42 +0900

    Title: 별표 출력하기 3, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241220/별표 출력하기 3/print-star-3.cpp
+++ 241220/별표 출력하기 3/print-star-3.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int a, b = 0, c = 0;
     cin >> a;
-    for(int i = 1 ; i <= 5 ; i++){
+    for(int i = 1 ; i <= a ; i++){
         for(int j = 0; j < b ; j++){
             cout << "  ";
         }
