5219ee82ea7e0ce90b4c08121fa12d48ffd523a4
Yuhahak <100738313+Yuhahak@users.noreply.github.com>
2024-12-23 08:58:00 +0900

    Title: 별 그리기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241223/별 그리기/star-drawing.cpp
+++ 241223/별 그리기/star-drawing.cpp
@@ -5,7 +5,7 @@ int main() {
     int a;
     cin >> a;
     for(int i = 0; i < a ; i++){
-        for(int j = 0; j < ((a - 1) - i); j++){
+        for(int j = 0; j < a- (i+1); j++){
             cout << " ";
         }
         for(int k = 0; k < ( 2 * i) + 1 ; k ++){
@@ -18,7 +18,7 @@ int main() {
         for(int k = 0; k < i + 1; k ++){
             cout << " ";
         }
-        for(int j = 0; j < a - ( 2 * i); j ++){
+        for(int j = 0; j < (2*a) - (2 * i) - 3; j ++){
             cout << "*";
         }
         cout << endl;
