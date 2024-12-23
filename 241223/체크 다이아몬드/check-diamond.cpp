7267723430f222545848a88400f4d624bff3be83
Yuhahak <100738313+Yuhahak@users.noreply.github.com>
2024-12-23 09:13:50 +0900

    Title: 체크 다이아몬드, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241223/체크 다이아몬드/check-diamond.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    for(int i = 0 ; i < a ; i++){
+        for(int j = 0 ; j < (a - 1) - i ; j ++){
+            cout << " ";
+        }
+        for(int k = 0 ; k < i + 1; k++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    for(int i = 0 ; i < a- 1; i++){
+        for(int j = 0; j < i+1 ; j++){
+            cout << " ";
+        }
+        for(int k = 0; k < (a - 1) - i; k ++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
