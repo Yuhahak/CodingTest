fbbf65e67a2a63cbc350b364442a908444d04642
Yuhahak <100738313+Yuhahak@users.noreply.github.com>
2024-12-26 08:51:54 +0900

    Title: 별표 출력하기 12, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241226/별표 출력하기 12/print-star-12.cpp
+++ 241226/별표 출력하기 12/print-star-12.cpp
@@ -9,7 +9,7 @@ int main() {
     }
     cout << endl;
     for(int i =0; i < a -1; i++){
-        for(int k = 0 ; k < a - 1; k++){
+        for(int k = 0 ; k < a; k++){
             if(k % 2 == 0){
                 cout << "  ";
             }
