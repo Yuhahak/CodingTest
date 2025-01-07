844e5255b5b8b454aa4683c35cbc11d3de395209
Yuhahak <100738313+Yuhahak@users.noreply.github.com>
2025-01-08 08:10:10 +0900

    Title: 반복 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250108/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+void PrintStr(int _num){
+    for(int i = 0 ; i < _num ; i ++){
+        cout << "12345^&*()_" << endl;
+    }
+}
+
+int main() {
+    cin >> N;
+    PrintStr(N);
+    // Write your code here!
+
+    return 0;
+}
